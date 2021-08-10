931696d6e0d9c9b6093dd68cf770c944b4f381a2
gaon000 <gaon000@naver.com>
2021-08-11 01:30:01 +0900

    part2 : components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class Header extends Component {
+  render() {
+    return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">hi</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +20,14 @@ function App() {
           Learn React
         </a>
       </header>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
     </div>
   );
 }
