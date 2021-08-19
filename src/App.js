d68e51a3b3fbb5a82f90395e82cc37d468f05715
gaon000 <gaon000@naver.com>
2021-08-20 01:46:43 +0900

    part9 form components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
 import './App.css';
 import React, { Component } from 'react';
-import ImageSlider from './components/ImageSlider';
-import Counter from './components/Counter';
-import Header from './components/Header'
-
+import MyForm from "./components/MyForm";
 
 class App extends Component {
 
   state = {
     visible: true,
-    whichComponentToShow: 'ImageSlider'
   }
 
   render() {
-    if (this.state.whichComponentToShow === 'ImageSlider') {
-      return (
-        <div className="App">
-          <ImageSlider />
-          <button onClick={() => {
-            this.setState({ whichComponentToShow: "Counter" })
-          }}>
-            show Counter
-          </button>
-        </div>
-      )
-    } else if (this.state.whichComponentToShow === 'Counter') {
-      return (
-        <div className="App">
-          <Counter />
-          <button onClick={() => {
-            this.setState({ whichComponentToShow: "Header" })
-          }}>
-            show Header
-          </button>
-        </div>
-      )
-    } else if (this.state.whichComponentToShow === 'Header') {
-      return (
-        <div className='App'>
-          <Header />
-        </div>
-      )
-    }
-
-    return null;
-
+    return (
+      <div className="App">
+        <MyForm />
+      </div>
+    )
   }
 }
 
